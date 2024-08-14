6f59e48f5f1cf75aa8056aaae145188af677c563
wyq0918dev <wyq0918dev@gmail.com>
2024-08-14 20:06:04 +0800

    commit

--- lib/freefeos_web.dart
+++ lib/freefeos_web.dart
@@ -1,10 +1,9 @@
-/// 平台插件注册和API导出
-library freefeos;
+/// Web插件注册
+library freefeos_web;
 
-export 'src/rust/api/simple.dart';
-export 'src/rust/frb_generated.dart' show RustLib;
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-/// 平台插件注册
+/// Web插件注册
 ///
 /// 插件注册由Flutter框架接管, 请勿手动注册.
 final class FreeFEOSRegister {
@@ -13,5 +12,5 @@ final class FreeFEOSRegister {
   /// 注册插件
   ///
   /// 插件注册由Flutter框架接管, 请勿手动注册.
-  static void registerWith() {}
+  static void registerWith(Registrar _) {}
 }
