8aeccfc150be06aad5f27dde19c2600b9b7d1ae1
wyq0918dev <wyq0918dev@gmail.com>
2024-08-30 04:06:23 +0800

    commit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -51,6 +51,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
         title: const Text('Home'),
       ),
       body: Center(
@@ -105,6 +106,7 @@ class _DetailsPageState extends State<DetailsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
         title: const Text('Details'),
       ),
       body: Center(
