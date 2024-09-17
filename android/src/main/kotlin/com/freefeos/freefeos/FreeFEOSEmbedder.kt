268afbf6bc3f65f975dad14a63c8210f7c723eda
wyq0918dev <wyq0918dev@gmail.com>
2024-09-18 00:16:25 +0800

    commit

--- android/src/main/kotlin/com/freefeos/freefeos/FreeFEOSEmbedder.kt
+++ android/src/main/kotlin/com/freefeos/freefeos/FreeFEOSEmbedder.kt
@@ -1,18 +1,3 @@
-/**
- * Copyright flutter_ecosed
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.freefeos.freefeos
 
 import android.app.Activity
@@ -41,7 +26,6 @@ import androidx.appcompat.app.AppCompatCallback
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.view.ActionMode
 import androidx.appcompat.widget.Toolbar
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
@@ -70,10 +54,10 @@ import rikka.shizuku.Shizuku
 
 /**
  * 作者: wyq0918dev
- * 仓库: https://github.com/libecosed/flutter_ecosed
+ * 仓库: https://github.com/freefeos/freefeos
  * 时间: 2024/01/28
  * 描述: flutter_ecosed
- * 文档: https://github.com/libecosed/flutter_ecosed/blob/master/README.md
+ * 文档: https://github.com/freefeos/freefeos/blob/master/README.md
  */
 class FreeFEOSEmbedder : Service(), FlutterPlugin, MethodChannel.MethodCallHandler,
     ActivityAware {
