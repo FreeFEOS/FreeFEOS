d58597d3ca0d1cc11ea49a3a91105ff341c8c89f
wyq0918dev <85048662+wyq0918dev@users.noreply.github.com>
2024-09-30 00:13:57 +0800

    commit

--- lib/src/widget/info.dart
+++ lib/src/widget/info.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../viewmodel/system_mmvm.dart';
 
 class InfoPage extends StatelessWidget {
   const InfoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Info'),
+    return Consumer<SystemViewModel>(
+      builder: (context, viewModel, child) => Scaffold(
+        appBar: AppBar(
+          title: Text('Info'),
+        ),
+        body: Column(
+          children: [
+            FilledButton(
+              onPressed: () {
+                viewModel.openApplication();
+              },
+              child: Text('app'),
+            ),
+            FilledButton(
+              onPressed: () {
+                viewModel.openManager();
+              },
+              child: Text('mgr'),
+            )
+          ],
+        ),
       ),
     );
   }
