cf9d7b2419955b7d5f9fa4935b1a9dcf784b8ab9
wyq0918dev <wyq0918dev@gmail.com>
2024-10-07 00:27:48 +0800

    commit

--- lib/src/widget/manager.dart
+++ lib/src/widget/manager.dart
@@ -433,6 +433,7 @@ class PluginPage extends StatefulWidget {
 }
 
 class _PluginPageState extends State<PluginPage> {
+  /// 滚动控制器
   final ScrollController _scrollController = ScrollController();
 
   @override
@@ -464,10 +465,13 @@ class _PluginPageState extends State<PluginPage> {
                 itemCount: viewModel.getPluginDetailsList.length,
                 padding: EdgeInsets.zero,
                 itemBuilder: (context, index) {
+                  // 首项
                   final PluginDetails first =
                       viewModel.getPluginDetailsList.first;
+                  // 尾项
                   final PluginDetails last =
                       viewModel.getPluginDetailsList.last;
+                  // 项
                   final PluginDetails details =
                       viewModel.getPluginDetailsList[index];
                   return Padding(
@@ -477,154 +481,140 @@ class _PluginPageState extends State<PluginPage> {
                       left: 12,
                       right: 12,
                     ),
-                    child: PluginCard(
-                      details: details,
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(24, 16, 24, 8),
+                        child: Consumer<SystemViewModel>(
+                          builder: (context, viewModel, child) => Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          details.title,
+                                          textAlign: TextAlign.start,
+                                          style: TextStyle(
+                                            fontSize: Theme.of(
+                                              context,
+                                            ).textTheme.titleMedium?.fontSize,
+                                            fontFamily: Theme.of(
+                                              context,
+                                            ).textTheme.titleMedium?.fontFamily,
+                                            height: Theme.of(
+                                              context,
+                                            ).textTheme.bodySmall?.height,
+                                            fontWeight: FontWeight.bold,
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                        ),
+                                        Text(
+                                          '${IntlLocalizations.of(context).managerPluginChannel}: ${details.channel}',
+                                          textAlign: TextAlign.start,
+                                          style: TextStyle(
+                                            fontSize: Theme.of(
+                                              context,
+                                            ).textTheme.bodySmall?.fontSize,
+                                            fontFamily: Theme.of(
+                                              context,
+                                            ).textTheme.bodySmall?.fontFamily,
+                                            height: Theme.of(
+                                              context,
+                                            ).textTheme.bodySmall?.height,
+                                          ),
+                                        ),
+                                        Text(
+                                          '${IntlLocalizations.of(context).managerPluginAuthor}: ${details.author}',
+                                          textAlign: TextAlign.start,
+                                          style: TextStyle(
+                                            fontSize: Theme.of(
+                                              context,
+                                            ).textTheme.bodySmall?.fontSize,
+                                            fontFamily: Theme.of(
+                                              context,
+                                            ).textTheme.bodySmall?.fontFamily,
+                                            height: Theme.of(
+                                              context,
+                                            ).textTheme.bodySmall?.height,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                    child: viewModel.getPluginIcon(
+                                      context,
+                                      details,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 12),
+                              Text(
+                                details.description,
+                                textAlign: TextAlign.start,
+                                style: Theme.of(
+                                  context,
+                                ).textTheme.bodySmall?.apply(
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                maxLines: 4,
+                              ),
+                              const SizedBox(height: 16),
+                              const Divider(),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    flex: 1,
+                                    child: Text(
+                                      viewModel.getPluginType(
+                                        context,
+                                        details,
+                                      ),
+                                      textAlign: TextAlign.start,
+                                      style: Theme.of(
+                                        context,
+                                      ).textTheme.bodySmall,
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 0,
+                                    child: Tooltip(
+                                      message: viewModel.getPluginTooltip(
+                                        context,
+                                        details,
+                                      ),
+                                      child: TextButton(
+                                        onPressed: viewModel.openPlugin(
+                                          context,
+                                          details,
+                                        ),
+                                        child: Text(
+                                          viewModel.getPluginAction(
+                                            context,
+                                            details,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
                   );
                 },
               ),
             ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class PluginCard extends StatelessWidget {
-  const PluginCard({
-    super.key,
-    required this.details,
-  });
-
-  final PluginDetails details;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(24, 16, 24, 8),
-        child: Consumer<SystemViewModel>(
-          builder: (context, viewModel, child) => Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          details.title,
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                            fontSize: Theme.of(
-                              context,
-                            ).textTheme.titleMedium?.fontSize,
-                            fontFamily: Theme.of(
-                              context,
-                            ).textTheme.titleMedium?.fontFamily,
-                            height: Theme.of(
-                              context,
-                            ).textTheme.bodySmall?.height,
-                            fontWeight: FontWeight.bold,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        Text(
-                          '${IntlLocalizations.of(context).managerPluginChannel}: ${details.channel}',
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                            fontSize: Theme.of(
-                              context,
-                            ).textTheme.bodySmall?.fontSize,
-                            fontFamily: Theme.of(
-                              context,
-                            ).textTheme.bodySmall?.fontFamily,
-                            height: Theme.of(
-                              context,
-                            ).textTheme.bodySmall?.height,
-                          ),
-                        ),
-                        Text(
-                          '${IntlLocalizations.of(context).managerPluginAuthor}: ${details.author}',
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                            fontSize: Theme.of(
-                              context,
-                            ).textTheme.bodySmall?.fontSize,
-                            fontFamily: Theme.of(
-                              context,
-                            ).textTheme.bodySmall?.fontFamily,
-                            height: Theme.of(
-                              context,
-                            ).textTheme.bodySmall?.height,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    child: viewModel.getPluginIcon(
-                      context,
-                      details,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 12),
-              Text(
-                details.description,
-                textAlign: TextAlign.start,
-                style: Theme.of(
-                  context,
-                ).textTheme.bodySmall?.apply(
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                maxLines: 4,
-              ),
-              const SizedBox(height: 16),
-              const Divider(),
-              Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Text(
-                      viewModel.getPluginType(
-                        context,
-                        details,
-                      ),
-                      textAlign: TextAlign.start,
-                      style: Theme.of(
-                        context,
-                      ).textTheme.bodySmall,
-                    ),
-                  ),
-                  Expanded(
-                    flex: 0,
-                    child: Tooltip(
-                      message: viewModel.getPluginTooltip(
-                        context,
-                        details,
-                      ),
-                      child: TextButton(
-                        onPressed: viewModel.openPlugin(
-                          context,
-                          details,
-                        ),
-                        child: Text(
-                          viewModel.getPluginAction(
-                            context,
-                            details,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
           ),
         ),
       ),
